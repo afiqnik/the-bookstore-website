06f2ea971839fa8bbd79933c9c2f0c4fd2b22267
Anas <anasfurdaus@gmail.com>
2024-08-04 01:54:57 +0800

    added search bar functionality

--- src/main/java/com/YP/bookstore/repository/ProductRepository.java
+++ src/main/java/com/YP/bookstore/repository/ProductRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> { // JpaRepository<entity, data type of primary key of entity>
     List<Product> findByIsBestsellerTrue();
     List<Product> findByIsNewArrivalTrue();
+    List<Product> findByTitleContainingIgnoreCaseOrAuthorContainingIgnoreCase(String title, String Author);
 }

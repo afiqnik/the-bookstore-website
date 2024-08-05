531d80764d9b4ca93171fdabed6c1258bc091f5d
Nik Afiq <afiqharipin@gmail.com>
2024-08-06 06:33:09 +0800

    Remove bugs & improve HTML files layout

--- src/main/java/com/YP/bookstore/service/ProductService.java
+++ src/main/java/com/YP/bookstore/service/ProductService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class ProductService {
+public class  ProductService {
     @Autowired
     private ProductRepository productRepository;
 

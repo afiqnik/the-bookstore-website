67596cbb09a502f89465a5a80890901bc6ff5d96
hendisantika <hendisantika@yahoo.co.id>
2025-03-10 05:50:09 +0700

    #8 | Hendi | feat: Add Simple Integration Test🫸🌀✏️📗🐧🐳⬆

--- src/test/java/com/YP/bookstore/BookstoreApplicationTests.java
+++ src/test/java/com/YP/bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,44 @@
 package com.YP.bookstore;
 
-import org.junit.jupiter.api.Test;
+import com.YP.bookstore.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@Testcontainers
+@SpringBootTest(
+		properties = {
+				"management.endpoint.health.show-details=always",
+				"spring.datasource.url=jdbc:tc:mysql:9.2.0:///bookstore"
+		},
+		webEnvironment = RANDOM_PORT
+)
 class BookstoreApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	private UserRepository userRepository;
+
+	@BeforeEach
+	void deleteAll() {
+		userRepository.deleteAll();
 	}
+
+//	@Test
+//	void checkUserIsExist() {
+//		// given
+//		var user = userRepository.saveAndFlush(
+//				new User("yuji", "53cret", "Itadori Yuji", "yuji@yopmail.com", "Tokyo", "1234-5678-9012-3456")
+//		);
+//
+//		// when
+//		var userOptional = userRepository.findByUsername("yuji");
+//
+//		// then
+//		assertThat(userOptional).isNotNull();
+//		assertThat(userOptional.getUsername()).isEqualTo(user.getId());
+//	}
 
 }

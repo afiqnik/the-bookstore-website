ac182b68b2a02a2888b69d4ccd26af05bd61b5a6
Nik Afiq <afiqharipin@gmail.com>
2025-03-12 12:11:54 +0800

    Update database schema & entity fields

--- src/main/java/com/YP/bookstore/model/Product.java
+++ src/main/java/com/YP/bookstore/model/Product.java
@@ -2,7 +2,6 @@ package com.YP.bookstore.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-
 import java.util.Date;
 
 /**
@@ -15,56 +14,33 @@ import java.util.Date;
 @Data
 public class Product {
 
-    /**
-     * The unique identifier for each product.
-     * It is annotated with @Id to denote the primary key.
-     * The @GeneratedValue annotation specifies that the ID should be generated
-     * automatically.
-     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // The title of the book.
     private String title;
-
-    // The author of the book.
     private String author;
-
-    // The publisher of the book.
     private String publisher;
-
-    // The date when the book was published.
     private Date publishedDate;
 
-    // A brief description of the book.
     @Column(columnDefinition = "TEXT")
     private String description;
 
-    // The ISBN identifier of the book.
     private String isbnIdentifier;
-
-    // The number of pages in the book.
     private Integer pageCount;
-
-    // The categories or genres the book belongs to.
     private String categories;
-
-    // The language in which the book is written.
     private String language;
-
-    // A URL or path to the image of the book's cover.
     private String imageLinks;
 
-    // The list price of the book.
+    @Column(name = "list_price")
     private Double listPrice;
 
-    // The retail price of the book.
+    @Column(name = "retail_price")
     private Double retailPrice;
 
-    // A flag indicating whether the book is a bestseller.
+    @Column(name = "is_bestseller")
     private Boolean isBestseller;
 
-    // A flag indicating whether the book is a new arrival.
+    @Column(name = "is_new_arrival")
     private Boolean isNewArrival;
 }
--- src/main/resources/db/migration/V1_09032025_1730__Init_Product_Data.sql
+++ src/main/resources/db/migration/V1_09032025_1730__Init_Product_Data.sql
@@ -1,19 +1,19 @@
-CREATE TABLE `product`
+CREATE TABLE product
 (
-    `id`              bigint NOT NULL AUTO_INCREMENT,
-    `title`           varchar(255) DEFAULT NULL,
-    `author`          varchar(255) DEFAULT NULL,
-    `publisher`       varchar(255) DEFAULT NULL,
-    `published_date`  datetime(6) DEFAULT NULL,
-    `description`     text,
-    `isbn_identifier` varchar(255) DEFAULT NULL,
-    `page_Count`      int          DEFAULT NULL,
-    `categories`      varchar(255) DEFAULT NULL,
-    `language`        varchar(255) DEFAULT NULL,
-    `image_links`     varchar(255) DEFAULT NULL,
-    `list_Price` DOUBLE DEFAULT NULL,
-    `retail_Price` DOUBLE DEFAULT NULL,
-    `is_Bestseller`   tinyint(1) DEFAULT NULL,
-    `is_New_Arrival`  tinyint(1) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=234 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    id              BIGINT NOT NULL AUTO_INCREMENT,
+    title           VARCHAR(255) DEFAULT NULL,
+    author          VARCHAR(255) DEFAULT NULL,
+    publisher       VARCHAR(255) DEFAULT NULL,
+    published_date  DATETIME(6) DEFAULT NULL,
+    description     TEXT,
+    isbn_identifier VARCHAR(255) DEFAULT NULL,
+    page_count      INT DEFAULT NULL,
+    categories      VARCHAR(255) DEFAULT NULL,
+    language        VARCHAR(255) DEFAULT NULL,
+    image_links     VARCHAR(255) DEFAULT NULL,
+    list_price      DOUBLE DEFAULT NULL,
+    retail_price    DOUBLE DEFAULT NULL,
+    is_bestseller   TINYINT(1) DEFAULT NULL,
+    is_new_arrival  TINYINT(1) DEFAULT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
